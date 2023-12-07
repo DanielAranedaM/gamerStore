31419748e598f2b917dd953698424b701e67c9de
DanielAranedaM <d.aranedam@duocuc.cl>
2023-12-07 19:18:04 -0300

    registro

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { User } from 'src/app/models/user.mode';
+import { FirebaseService } from 'src/app/services/firebase.service';
 
 @Component({
   selector: 'app-login',
@@ -13,14 +15,20 @@ export class LoginPage implements OnInit {
     email: new FormControl('', [Validators.email, Validators.required]),
     contrasena: new FormControl('', [Validators.required])
   })
-  constructor() {}
+
+  //**Inyectando servicio de firebase*/
+firebaseSvc = inject(FirebaseService);
 
   ngOnInit(){
 
   }
   
   submit(){
-    console.log(this.form.value);
+    if(this.form.valid){
+      this.firebaseSvc.singIn(this.form.value as User).then(res => {
+        console.log(res);
+      })
+    };
   }
   
 }
