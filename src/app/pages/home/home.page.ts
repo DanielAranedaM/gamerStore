058601fe27cff9bed81aba9c24bba9361fe526f4
sebastianaraneda1 <sebastian_lb18@outlook.cl>
2023-12-07 16:45:18 -0300

    Modificaciones en el componente add y actualizar

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from 'src/app/services/firebase.service';
+import { UtilsService } from 'src/app/services/util.service';
+import { AddUpdateProductoComponent } from 'src/app/shared/components/add-update-producto/add-update-producto.component';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private firebaseSvc:FirebaseService,
+    private utilsSvc:UtilsService
+    ) { }
 
   ngOnInit() {
+  }
+
+  //Salir de la cuenta
+  cerrarSesion(){
+    this.firebaseSvc.auth.signOut();
+  }
+
+  //Agregar y actualizar el producto
+  actualizarProducto(){
+    this.utilsSvc.mostrarModal({
+      component: AddUpdateProductoComponent,
+      cssClass: 'add-update-modal'
+    })
   }
 
 }
